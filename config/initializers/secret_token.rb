1621aecfcfad144dafabd5705f95b8f54229ae30
dbc-apprentice <jamescarney10@gmail.com>
2013-08-07 10:12:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackClone::Application.config.secret_token = '539bf2397bf169122aaed133c0e1084e106c1d06063b27977b72cc3398b97825694ac2d4b490a399adc97d59efb4c30d685d17aee64e0157f627501c754e8e72'
