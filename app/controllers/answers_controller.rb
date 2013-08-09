b2e4f9020fea2acc466e340ad27f10500d8c9d73
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-08-08 22:13:04 -0500

    answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,43 @@
 class AnswersController < ApplicationController
+
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    @answer = Answer.find_by_id(params[:id])
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = current_user.answer.new(params[:answer])
+    if @answer.valid?
+      @answer.save
+      redirect_to question_path(@answer.question)
+    else
+      @errors = @answer.errors.full_message
+      render question_path(@answer.question)
+    end
+  end
+
+  def destroy
+    answer = Answer.find_by_id(params[:id])
+    question = answer.question
+    Answer.destroy(params[:id])
+    redirect_to questions_path(@question)
+  end
+  
+  def update
+    @answer = Answer.find_by_id(params[:id])
+    @answer.update_attributes(params[:answer])
+    redirect_to question_path(@answer.question)
+  end
+
 end
