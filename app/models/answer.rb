6cadbc03b05eab192d9d600b7a0be91b5d8ab4cb
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 23:00:19 -0500

    Update answer model associations to incorporate comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,11 @@
 class Answer < ActiveRecord::Base
-
   attr_accessible :user_id, :question_id, :text
   validates :user_id, presence: true
   validates :text, presence: true
 
   belongs_to :user
   belongs_to :question
+  has_many :comments, as: :commentable
+
 
 end
